9784f36d32d5709afe36b32b8873962ce96204df
garber <garbfbatista@gmail.com>
2023-03-01 18:34:34 -0400

    tarea-7

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View, StyleSheet, Button, Image, Container } from "react-native";
+import { Audio } from "expo-av";
 
 export default function App() {
+  const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/Hello.mp3")
+    );
+    setSound(sound);
+
+    await sound.playAsync();
+  }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  //funcio detener sonido
+  async function stopSound() {
+    await sound.stopAsync();
+  }
+
   return (
+    //reproducir el sonido y colocar imagen
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={styles.tinyLogo}
+        source={require("./assets/maxresdefault.jpg")}
+      />
+      <Text style={{ textAlign: "center", fontSize: 20 }}>
+        Audio con React Native
+      </Text>
+
+      <Button title='Reproducir sonido' onPress={playSound} />
     </View>
   );
 }
@@ -14,8 +47,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    paddingTop: 50,
+    backgroundColor: "#ecf0f1",
+    padding: 8,
   },
+  //centrar imagen
+  tinyLogo: {
+    width: 300,
+    height: 300,
+    alignSelf: "center",
+  },
+  //espacio entre botones
 });
